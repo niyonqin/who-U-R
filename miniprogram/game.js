629367a494477335f100224a8c38a706f7bc929f
youkiNi <527678809@qq.com>
2019-11-11 09:50:26 +0800

    update

--- miniprogram/game.js
+++ miniprogram/game.js
@@ -1,6 +1,25 @@
-import './js/libs/weapp-adapter'
-import './js/libs/symbol'
-
-import Main from './js/main'
-
-new Main()
+Game({
+  onLaunch: function () {
+  },
+  getUserInfo: function (cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData: {
+    userInfo: null
+  }
+})
